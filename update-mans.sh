bf3652b3f78e8d83938b03691a68a5dee664eee5
Florian Forster <octo@verplant.org>
2017-05-10 10:04:31 +0200

    Fix timezone format in manpages' front matter.

--- update-mans.sh
+++ update-mans.sh
@@ -3,6 +3,10 @@
 declare -r SRCDIR='/home/octo/liboping/src/mans'
 declare -r DSTDIR='./content/documentation'
 
+# date --iso-8601=seconds formats the time zone without a colon between hour
+# and minute, which is confusing Hugo.
+declare -r DATE="$(date +"%FT%T%:z")"
+
 while read infile; do
 	name="$(basename "${infile}" .pod)"
 	outfile="${DSTDIR}/${name}.md"
@@ -14,7 +18,7 @@ while read infile; do
 
 	(
 		echo '---'
-		echo 'date:' $(date --iso-8601=seconds)
+		echo "date: \"${DATE}\""
 		echo "title: \"${name}(${section})\""
 		echo 'menu:'
 		echo '  main:'
